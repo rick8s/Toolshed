ad477daf36f404dc32237a02ef0a80d26e2452a4
Rick8s <rick8s@hotmail.com>
2015-12-11 14:31:46 -0600

    generated class Tools

---
+++ Toolshed/Models/Tool.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+
+namespace Toolshed.Models 
+{
+    public class Tool 
+    {
+        public string Category { get; set; }
+        public string Content { get; set; }
+        public object Owner { get; set; }
+        public string Picture { get; set; }
+        [Key]
+        public int ToolId { get; set; }
+    }
+}
