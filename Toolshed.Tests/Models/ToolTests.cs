bda2fd816bd21ebfd70e80b61a175ef8808aa314
Rick8s <rick8s@hotmail.com>
2015-12-11 14:25:26 -0600

    added Models folder to hold tests

---
+++ Toolshed.Tests/Models/ToolTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toolshed.Models;
+
+namespace Toolshed.Tests.Models
+{
+    [TestClass]
+    public class ToolTests
+    {
+        [TestMethod]
+        public void ToolEnsureICanCreateAnInstance()
+        {
+            Tool a_tool = new Tool();
+
+            Assert.IsNotNull(a_tool);
+        }
+
+        [TestMethod]
+        public void ToolEnsureAToolHasAllItsInfo()
+        {
+            //Arrange
+            Tool a_tool = new Tool();
+
+            // Act 
+            a_tool.ToolId = 1;
+            a_tool.Content = "My Content";
+            a_tool.Owner = null; // Will need to define this later
+            a_tool.Picture = "https://google.com";
+            a_tool.Category = "A Category";
+
+            // Assert
+            Assert.AreEqual(1, a_tool.ToolId);
+            Assert.AreEqual("My Content", a_tool.Content);
+            Assert.AreEqual(null, a_tool.Owner);
+            Assert.AreEqual("https://google.com", a_tool.Picture);
+            Assert.AreEqual("A Category", a_tool.Category);
+        }
+    }
+}
