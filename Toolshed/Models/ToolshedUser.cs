e1697d6b1cb1f6186a505549e6d472593d01296c
Rick8s <rick8s@hotmail.com>
2015-12-14 15:45:54 -0600

    added UserName to ToolshedUser class

--- Toolshed/Models/ToolshedUser.cs
+++ Toolshed/Models/ToolshedUser.cs
@@ -12,6 +12,11 @@ namespace Toolshed.Models
         public string FirstName { get; set; }
         [Required]
         public string LastName { get; set; }
+        [Required]
+        [MaxLength(15)]
+        [MinLength(3)]
+        [RegularExpression(@"^[a-zA-Z\d]+[-_a-zA-Z\d]{0,2}[a-zA-Z\d]+")]
+        public string UserName { get; set; }
         public string Phone { get; set; }
         [Required]
         public string Street { get; set; }
