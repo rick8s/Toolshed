509e3ba4c404fa1951cdbd828981f1123040ef2a
Rick8s <rick8s@hotmail.com>
2015-12-14 15:49:18 -0600

    Created utility method for connecting mocks to data store. Using TestInitialize and Cleanup

--- Toolshed.Tests/Models/ToolshedRepositoryTests.cs
+++ Toolshed.Tests/Models/ToolshedRepositoryTests.cs
@@ -10,17 +10,54 @@ namespace Toolshed.Tests.Models
     [TestClass]
     public class ToolshedRepositoryTests
     {
+
+        private Mock<ToolshedContext> mock_context;
+        private Mock<DbSet<ToolshedUser>> mock_set;
+        //private Mock<DbSet<Tool>> mock_tool_set;
+        private ToolshedRepository repository;
+
+        private void ConnectMocksToDataStore(IEnumerable<ToolshedUser> data_store)
+        {
+            var data_source = data_store.AsQueryable<ToolshedUser>();
+            // HINT HINT: var data_source = (data_store as IEnumerable<ToolshedUser>).AsQueryable();
+            // Convince LINQ that our Mock DbSet is a (relational) Data store.
+            mock_set.As<IQueryable<ToolshedUser>>().Setup(data => data.Provider).Returns(data_source.Provider);
+            mock_set.As<IQueryable<ToolshedUser>>().Setup(data => data.Expression).Returns(data_source.Expression);
+            mock_set.As<IQueryable<ToolshedUser>>().Setup(data => data.ElementType).Returns(data_source.ElementType);
+            mock_set.As<IQueryable<ToolshedUser>>().Setup(data => data.GetEnumerator()).Returns(data_source.GetEnumerator());
+
+            // This is Stubbing the ToolshedUsers property getter
+            mock_context.Setup(a => a.ToolshedUsers).Returns(mock_set.Object);
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            mock_context = new Mock<ToolshedContext>();
+            mock_set = new Mock<DbSet<ToolshedUser>>();
+            //mock_tool_set = new Mock<DbSet<Tool>>();
+            repository = new ToolshedRepository(mock_context.Object);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            mock_context = null;
+            mock_set = null;
+           // mock_tool_set = null;
+            repository = null;
+        }
+
         [TestMethod]
         public void ToolshedContextEnsureICanCreateInstance()
         {
-            ToolshedContext context = new ToolshedContext();
+            ToolshedContext context = mock_context.Object;
             Assert.IsNotNull(context);
         }
 
         [TestMethod]
         public void ToolshedRepositoryEnsureICanCreateInstance()
         {
-            ToolshedRepository repository = new ToolshedRepository();
             Assert.IsNotNull(repository);
         }
 
@@ -30,28 +67,20 @@ namespace Toolshed.Tests.Models
             //Arrange
             var expected = new List<ToolshedUser>
             {
-                new ToolshedUser { FirstName = "Rick"  },
-                new ToolshedUser { FirstName = "Kyle" },
-                new ToolshedUser {FirstName = "Doug" }
+                new ToolshedUser { UserName = "Rick8s"  },
+                new ToolshedUser { UserName = "Kyle" },
+                new ToolshedUser { UserName = "Doug" }
             };
-            Mock<ToolshedContext> mock_context = new Mock<ToolshedContext>();
-            Mock<DbSet<ToolshedUser>> mock_set = new Mock<DbSet<ToolshedUser>>();
 
             mock_set.Object.AddRange(expected);
-            var data_source = expected.AsQueryable();
 
-            mock_set.As<IQueryable<ToolshedUser>>().Setup(data => data.Provider).Returns(data_source.Provider);
-            mock_set.As<IQueryable<ToolshedUser>>().Setup(data => data.Expression).Returns(data_source.Expression);
-            mock_set.As<IQueryable<ToolshedUser>>().Setup(data => data.ElementType).Returns(data_source.ElementType);
-            mock_set.As<IQueryable<ToolshedUser>>().Setup(data => data.GetEnumerator()).Returns(data_source.GetEnumerator());
-
-            mock_context.Setup(a => a.ToolshedUsers).Returns(mock_set.Object);
-            ToolshedRepository repository = new ToolshedRepository(mock_context.Object);
+            ConnectMocksToDataStore(expected);
 
             //Act
             var actual = repository.GetAllUsers();
 
             //Assert
+            Assert.AreEqual("Rick8s", actual.First().UserName);
             CollectionAssert.AreEqual(expected, actual);
 
         }
@@ -59,9 +88,6 @@ namespace Toolshed.Tests.Models
         [TestMethod]
         public void ToolshedRepositoryEnsureIHaveAContext()
         {
-            //Arrange
-            ToolshedRepository repository = new ToolshedRepository();
-
             //Act
             var actual = repository.Context;
 
