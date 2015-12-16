36cd646dc3a46eaac6613c08cd7aad3e4d0e331b
Rick8s <rick8s@hotmail.com>
2015-12-16 14:53:18 -0600

    Added InitialCreate Migration file

---
+++ Toolshed/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace Toolshed.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Toolshed.Models.ToolshedContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Toolshed.Models.ToolshedContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
